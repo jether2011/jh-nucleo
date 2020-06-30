5dd5f453adfcff19c4148374569b9a36872fe171
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-06-29 22:20:54 -0300

    Initial version of nucleo generated by JHipster-6.10.0

---
+++ src/main/java/org/tempestade/nucleo/service/InvalidPasswordException.java
@@ -0,0 +1,11 @@
+package org.tempestade.nucleo.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
